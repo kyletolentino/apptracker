cd1a476543f7a906b61b2a879f35f31bda00b194
Kyle Tolentino <kyltol@gmail.com>
2020-05-18 13:35:00 -0500

    testing commit

--- tracker.py
+++ tracker.py
@@ -10,12 +10,12 @@ company_label.grid(row=0, column=0, sticky=W)
 company_entry = Entry(app, textvariable=company_text)
 company_entry.grid(row=1, column=0)
 
-# job id
-id_text = StringVar()
-id_label = Label(app, text='Job ID', font=('bold', 14), pady=20)
-id_label.grid(row=0, column=1, sticky=W)
-id_entry = Entry(app, textvariable=id_text)
-id_entry.grid(row=1, column=1)
+# # job id
+# id_text = StringVar()
+# id_label = Label(app, text='Job ID', font=('bold', 14), pady=20)
+# id_label.grid(row=0, column=1, sticky=W)
+# id_entry = Entry(app, textvariable=id_text)
+# id_entry.grid(row=1, column=1)
 
 app.title('Application Tracker')
 app.geometry('800x400')
